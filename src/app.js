9914b380bea6a58a66f4703eb6b49de6a72b3541
unknown <tereza.raidlova@gmail.com>
2020-07-17 11:17:13 +0200

    svg 1

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// function addLi() {
+//   const things = ["tacos", "lol", "blah"];
+//   let items = "";
+
+//   for (let i = 0; i < things.length; i++) {
+//     debugger;
+//     items = items + `<li>${things[i]}</li>`;
+//   }
+//   const container = document.getElementById("container");
+//   container.innerHTML = `<ul>${items}</ul>`;
+// }
+
+// addLi();
